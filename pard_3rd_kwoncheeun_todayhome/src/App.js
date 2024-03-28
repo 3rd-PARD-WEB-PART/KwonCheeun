32c4e014980fad9c03d95bd60f33c0c20855d83d
codms0201 <chanteol0615@naver.com>
2024-03-28 16:06:26 +0900

    today_house

--- pard_3rd_kwoncheeun_todayhome/src/App.js
+++ pard_3rd_kwoncheeun_todayhome/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./img/오늘의집_logo.png";
+import RegisterPage from './Pages/RegisterPage/RegisterPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <img src={logo} className='logo' alt='logo_pic' />
+  </div>
 }
 
 export default App;
