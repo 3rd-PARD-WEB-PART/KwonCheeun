8a34beb9d5e41ded92a1a320adbef1d6c3c00973
codms0201 <chanteol0615@naver.com>
2024-03-29 13:23:03 +0900

    Finally

--- pard_3rd_kwoncheeun_todayhome/src/App.js
+++ pard_3rd_kwoncheeun_todayhome/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import logo from "./img/오늘의집_logo.png";
-import RegisterPage from './Pages/RegisterPage/RegisterPage'
+import RegisterPage from "./Pages/RegisterPage/RegisterPage";
 
 function App() {
-  <div>
-    <img src={logo} className='logo' alt='logo_pic' />
-  </div>
+  return(
+    <div className="App">
+      <div className="container">
+        <img src={logo} className='logo' alt='logo_pic' />
+        <RegisterPage />
+      </div>
+    </div>
+
+  );
 }
 
 export default App;
--- pard_3rd_kwoncheeun_todayhome/src/Pages/RegisterPage/RegisterPage.js
+++ pard_3rd_kwoncheeun_todayhome/src/Pages/RegisterPage/RegisterPage.js
@@ -1,6 +1,50 @@
+import React, { useState } from 'react';
 
 function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [nickname, setNickname] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("이메일:", email);
+        console.log("비밀번호:", password);
+        console.log("비밀번호 확인:", passwordCheck);
+        console.log("닉네임:", nickname);
+    };
+
+  return (
+    <div className="register-container">
+        <h2>회원가입</h2>
+        <form onSubmit={handleSubmit}>
+            <div className="input-container">
+                <h3>이메일</h3>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일" />
+            </div>
+
+            <div className="input-container">
+                <h3>비밀번호</h3>
+                <p>영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.</p>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" />
+            </div>
+
+            <div className="input-container">
+                <h3>비밀번호 확인</h3>
+                <input type="password" value={passwordCheck} onChange={(e) => setPasswordCheck(e.target.value)} placeholder="비밀번호 확인" />
+            </div>
+
+            <div className="input-container">
+                <h3>닉네임</h3>
+                <p>다른 유저와 겹치지 않도록 입력해주세요. (2~15자)</p>
+                <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} placeholder="별명 (2~15자)" />
+            </div>
+
+            <button className="btn">회원가입하기</button>
+        </form>
+        <h4>이미 아이디가 있으신가요? <span style={{textDecoration: "underline"}}>로그인</span></h4>
+    </div>
+    );
 }
 
 export default RegisterPage;
